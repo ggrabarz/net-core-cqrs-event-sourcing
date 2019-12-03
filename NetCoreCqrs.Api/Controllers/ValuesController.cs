cac10678aa0727fa61dd9c577ec82a97b590f616
Paweł Wichary <wicharypawel@gmail.com>
2019-12-03 14:04:54 +0100

    Add controller and register services

--- NetCoreCqrs.Api/Controllers/ValuesController.cs
+++ NetCoreCqrs.Api/Controllers/ValuesController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NetCoreCqrs.Api.Core;
+using System;
 
 namespace NetCoreCqrs.Api.Controllers
 {
@@ -6,10 +8,60 @@ namespace NetCoreCqrs.Api.Controllers
     [ApiController]
     public class ValuesController : ControllerBase
     {
-        [HttpGet]
-        public IActionResult Get()
+        private ICommandSender _bus;
+        private readonly IReadModelFacade _readmodel;
+
+        public ValuesController(IReadModelFacade readmodel, ICommandSender bus)
         {
-            return Ok("Hello World");
+            _readmodel = readmodel;
+            _bus = bus;
+        }
+
+        [HttpGet]
+        public ActionResult GetInventoryItems()
+        {
+            return Ok(_readmodel.GetInventoryItems());
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult GetItemDetails([FromRoute] Guid id)
+        {
+            return Ok(_readmodel.GetInventoryItemDetails(id));
+        }
+
+        [HttpPost]
+        public ActionResult AddItem([FromQuery] string name)
+        {
+            _bus.Send(new CreateInventoryItem(Guid.NewGuid(), name));
+            return StatusCode(201);
+        }
+
+        [HttpPost("{id}/ChangeName")]
+        public ActionResult ChangeName([FromRoute] Guid id, [FromQuery] string name, [FromQuery] int version)
+        {
+            _bus.Send(new RenameInventoryItem(id, name, version));
+            return NoContent();
+        }
+
+        [HttpPost("{id}/Deactivate")]
+        public ActionResult Deactivate([FromRoute] Guid id, [FromQuery] int version)
+        {
+            _bus.Send(new DeactivateInventoryItem(id, version));
+            return NoContent();
+        }
+
+        [HttpPost("{id}/CheckIn")]
+        public ActionResult CheckIn([FromRoute] Guid id, [FromQuery] int number, [FromQuery] int version)
+        {
+            _bus.Send(new CheckInItemsToInventory(id, number, version));
+            return NoContent();
+        }
+
+        [HttpPost("{id}/Remove")]
+        public ActionResult Remove([FromRoute] Guid id, [FromQuery] int number, [FromQuery] int version)
+        {
+            _bus.Send(new RemoveItemsFromInventory(id, number, version));
+            return NoContent();
         }
     }
 }
--- NetCoreCqrs.Api/Startup.cs
+++ NetCoreCqrs.Api/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using NetCoreCqrs.Api.Core;
 
 namespace NetCoreCqrs.Api
 {
@@ -15,7 +16,9 @@ namespace NetCoreCqrs.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IReadModelFacade, ReadModelFacade>();
             services.AddControllers();
+            Application_Start(services);
         }
 
         public void Configure(IApplicationBuilder app)
@@ -27,6 +30,31 @@ namespace NetCoreCqrs.Api
             {
                 endpoints.MapControllers();
             });
+        }
+
+        protected void Application_Start(IServiceCollection services)
+        {
+            var bus = new FakeBus();
+            var storage = new EventStore(bus);
+            var rep = new Repository<InventoryItem>(storage);
+            var commands = new InventoryCommandHandlers(rep);
+            bus.RegisterHandler<CheckInItemsToInventory>(commands.Handle);
+            bus.RegisterHandler<CreateInventoryItem>(commands.Handle);
+            bus.RegisterHandler<DeactivateInventoryItem>(commands.Handle);
+            bus.RegisterHandler<RemoveItemsFromInventory>(commands.Handle);
+            bus.RegisterHandler<RenameInventoryItem>(commands.Handle);
+            var detail = new InventoryItemDetailView();
+            bus.RegisterHandler<InventoryItemCreated>(detail.Handle);
+            bus.RegisterHandler<InventoryItemDeactivated>(detail.Handle);
+            bus.RegisterHandler<InventoryItemRenamed>(detail.Handle);
+            bus.RegisterHandler<ItemsCheckedInToInventory>(detail.Handle);
+            bus.RegisterHandler<ItemsRemovedFromInventory>(detail.Handle);
+            var list = new InventoryListView();
+            bus.RegisterHandler<InventoryItemCreated>(list.Handle);
+            bus.RegisterHandler<InventoryItemRenamed>(list.Handle);
+            bus.RegisterHandler<InventoryItemDeactivated>(list.Handle);
+            services.AddSingleton<ICommandSender>(bus);
+            services.AddSingleton<IEventPublisher>(bus);
         }
     }
 }
